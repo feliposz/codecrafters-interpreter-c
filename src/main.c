c3661afead5cfbfdee91d083b1351361a71937b8
feliposz <feliposz@gmail.com>
2025-05-10 05:47:36 -0300

    Scanning: Empty file

--- src/main.c
+++ src/main.c
@@ -4,33 +4,35 @@
 
 char *read_file_contents(const char *filename);
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     // Disable output buffering
     setbuf(stdout, NULL);
     setbuf(stderr, NULL);
 
-    if (argc < 3) {
+    if (argc < 3)
+    {
         fprintf(stderr, "Usage: ./your_program tokenize <filename>\n");
         return 1;
     }
 
     const char *command = argv[1];
 
-    if (strcmp(command, "tokenize") == 0) {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        fprintf(stderr, "Logs from your program will appear here!\n");
-        
+    if (strcmp(command, "tokenize") == 0)
+    {
         char *file_contents = read_file_contents(argv[2]);
 
-        // Uncomment this block to pass the first stage
-        // if (strlen(file_contents) > 0) {
-        //     fprintf(stderr, "Scanner not implemented\n");
-        //     exit(1);
-        // } 
-        // printf("EOF  null\n"); // Placeholder, remove this line when implementing the scanner
-        
+        if (strlen(file_contents) > 0)
+        {
+            fprintf(stderr, "Scanner not implemented\n");
+            exit(1);
+        }
+        printf("EOF  null\n"); // Placeholder, remove this line when implementing the scanner
+
         free(file_contents);
-    } else {
+    }
+    else
+    {
         fprintf(stderr, "Unknown command: %s\n", command);
         return 1;
     }
@@ -38,9 +40,11 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-char *read_file_contents(const char *filename) {
+char *read_file_contents(const char *filename)
+{
     FILE *file = fopen(filename, "r");
-    if (file == NULL) {
+    if (file == NULL)
+    {
         fprintf(stderr, "Error reading file: %s\n", filename);
         return NULL;
     }
@@ -50,14 +54,16 @@ char *read_file_contents(const char *filename) {
     rewind(file);
 
     char *file_contents = malloc(file_size + 1);
-    if (file_contents == NULL) {
+    if (file_contents == NULL)
+    {
         fprintf(stderr, "Memory allocation failed\n");
         fclose(file);
         return NULL;
     }
 
     size_t bytes_read = fread(file_contents, 1, file_size, file);
-    if (bytes_read < file_size) {
+    if (bytes_read < file_size)
+    {
         fprintf(stderr, "Error reading file contents\n");
         free(file_contents);
         fclose(file);
