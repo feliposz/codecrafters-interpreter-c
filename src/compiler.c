14b2345f79fde45d52cf731bbf408d556ecdb466
feliposz <feliposz@gmail.com>
2025-05-15 07:41:46 -0300

    For statements

--- src/compiler.c
+++ src/compiler.c
@@ -574,6 +574,58 @@ static void whileStatement()
     emitByte(OP_POP);
 }
 
+static void varDeclaration();
+
+static void forStatement()
+{
+    beginScope();
+    consume(TOKEN_LEFT_PAREN, "Expect '(' after 'for'.");
+    // initializer
+    if (match(TOKEN_SEMICOLON))
+    {
+        // empty
+    }
+    else if (match(TOKEN_VAR))
+    {
+        varDeclaration();
+    }
+    else
+    {
+        expressionStatement();
+    }
+    // condition
+    int loopStart = currentChunk()->count;
+    int exitJump = -1;
+    if (!match(TOKEN_SEMICOLON))
+    {
+        expression();
+        consume(TOKEN_SEMICOLON, "Expect ';' after loop condition.");
+        exitJump = emitJump(OP_JUMP_IF_FALSE);
+        emitByte(OP_POP);
+    }
+    // increment
+    if (!match(TOKEN_RIGHT_PAREN))
+    {
+        int bodyJump = emitJump(OP_JUMP);
+        int incrementStart = currentChunk()->count;
+        expression();
+        emitByte(OP_POP); // execute expression for side effect only, so pop value
+        consume(TOKEN_RIGHT_PAREN, "Expect ')' after for clauses.");
+        emitLoop(loopStart);
+        loopStart = incrementStart;
+        patchJump(bodyJump);
+    }
+    // body
+    statement();
+    emitLoop(loopStart);
+    if (exitJump != -1)
+    {
+        patchJump(exitJump);
+        emitByte(OP_POP); // pop condition expression value
+    }
+    endScope();
+}
+
 static void statement()
 {
     if (match(TOKEN_PRINT))
@@ -593,6 +645,10 @@ static void statement()
     else if (match(TOKEN_WHILE))
     {
         whileStatement();
+    }
+    else if (match(TOKEN_FOR))
+    {
+        forStatement();
     }
     else
     {
